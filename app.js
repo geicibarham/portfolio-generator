ec19e711ab3c96729d1fc7cd9dc0f701af3c2db6
geicibarham <geicianecosta43@gmail.com>
2022-02-20 18:12:15 -0500

    each file has a specific function

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// this method is called assigment destructuring and makes arrays for each one of those elements
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+// to start lets have function returning string, then in the end we will modify string into html
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+  console.log('Portoflio complete! check out index.html to see the output!')
+});
+// first argument on fs write file is file name
+// second argument is data that is be9ng written (in this case we used the function generate page)
+// third argument is callback fuction that will handle any errors
+// if error exists, throw a message error
